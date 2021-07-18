5c90d7568315f69b866f65c0374c6522227ac595
philip howley <howley.phil@gmail.com>
2021-07-18 12:07:50 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,20 @@
+//const { query } = require('express');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+//path to public folder so it can pick up the files
+app.use(express.static("public"));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
